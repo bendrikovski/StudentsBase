5c91c799790867e1653c6cdebdf9fdfbddf41390
bendrikovski <bendrikovski@gmail.com>
2020-05-07 20:20:37 +0300

    Fix repositories

--- src/main/java/com/ben/StudentsBase/repository/CourseRepository.java
+++ src/main/java/com/ben/StudentsBase/repository/CourseRepository.java
@@ -1,13 +1,12 @@
 package com.ben.StudentsBase.repository;
 
 import com.ben.StudentsBase.model.Course;
-import com.ben.StudentsBase.model.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository("courseRepository")
 public interface CourseRepository extends JpaRepository<Course, Long> {
-
-
     List<Course> findByDiscipline(String discipline);
 }
--- src/main/java/com/ben/StudentsBase/repository/HostelRepository.java
+++ src/main/java/com/ben/StudentsBase/repository/HostelRepository.java
@@ -2,7 +2,9 @@ package com.ben.StudentsBase.repository;
 
 import com.ben.StudentsBase.model.Hostel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository("hostelRepository")
 public interface HostelRepository extends JpaRepository<Hostel, Long> {
 
 }
